64e8df96d114b3a7002c354c67614688cc367121
York Yao <me@yorkyao.com>
2020-08-09 10:15:23 +0800

    build: improve release script

--- clean-release.config.ts
+++ clean-release.config.ts
@@ -1,4 +1,6 @@
-export default {
+import { Configuration } from 'clean-release'
+
+const config: Configuration = {
   include: [
     'src/index.d.ts',
     'LICENSE',
@@ -21,3 +23,5 @@ export default {
     `git push origin v${version}`
   ]
 }
+
+export default config
